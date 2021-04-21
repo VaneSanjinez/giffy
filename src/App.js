6037367978d9fa8969effb113f7a5fe8a4ce8cc8
VaneSanjinez <vane.sanji@gmail.com>
2021-04-20 22:26:47 -0400

    # Fetch from API 10 gifs

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 
-const GIFS = ['https://media3.giphy.com/media/12L6wFC7gHKQ92/giphy.gif?cid=ecf05e475xqs0l7c3xnh2rufixz0uj3896kfxa5o55gi7u2q&rid=giphy.gif&ct=g',
-              'https://media4.giphy.com/media/EPcvhM28ER9XW/giphy.gif?cid=ecf05e47ay6y90v1agxve7dxl7qor7raxld2jddxmxb04u65&rid=giphy.gif&ct=g']
+const apiUrl = 'https://api.giphy.com/v1/gifs/search?api_key=2AVXCSVVGMc70DwnTcdX2NXPXMrg9Wth&q=panda&limit=10&offset=0&rating=g&lang=en'
 
-const DIFF_GIFS = ['https://media0.giphy.com/media/BdMRkwomqVsLm/200w.webp?cid=ecf05e47a6wzo2njud4jhn4vm2j7fgb2m7refhfvdaoh2wwm&rid=200w.webp&ct=g']
 function App() {
-  // useEffect(function(){
-  //   console.log('effect ran');
-  //   setGifs(DIFF_GIFS)
-  // }, []) //component DidMount
+  const [gifs, setGifs] = useState([])
+  
+  useEffect(function(){
+    console.log('effect ran');
+    fetch(apiUrl)
+      .then(res => res.json())
+      .then(response => {
+        const {data} = response
+        const gifs = data.map(image => image.images.downsized_medium.url)
+        console.log(gifs)
+        setGifs(gifs)
+      })
+  }, []) //component DidMount
 
-  const [gifs, setGifs] = useState(GIFS)
+
   return (
     <div className="App">
       <section className="App-content">
-        {gifs.map(singleGif => <img src = {singleGif}/>)
+        {
+          gifs.map(singleGif => <img src = {singleGif}/>)
         }
-        <button onClick={() => setGifs(DIFF_GIFS)}>Change gif</button> 
       </section>
     </div>
   );
