513310f6feee95c1b7548f0c0d7ccdd1e7147020
VaneSanjinez <vane.sanji@gmail.com>
2021-04-21 22:14:22 -0400

    #Add component folder and move logic to clean App file

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
-import getGifs from './services/getGifs';
-
+import ListOfGifs from './components/ListOfGifs'
 
 
 
 function App() {
-  const [gifs, setGifs] = useState([])
-  
-  useEffect(function(){
-    getGifs({keyword : 'rugrats'}).then(gifs => setGifs(gifs))
-   
-  }, []) 
-
-
   return (
     <div className="App">
       <section className="App-content">
-        { 
-          gifs.map(singleGif => {
-            return <div>
-                <h4> {singleGif.title}</h4>
-                <small>{singleGif.id}</small>
-                <img alt = {singleGif.title}src = {singleGif.url}/>
-            </div>
-          })
-    }
+        <ListOfGifs keyword='ecuador'/>
       </section>
     </div>
   );
