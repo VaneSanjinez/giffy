fb3af5c9e29bcc04ca59a8b34f581401e8831def
VaneSanjinez <vane.sanji@gmail.com>
2021-04-24 22:46:29 -0400

    #Add eextra routes and converted to a SPA with Link from wouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import ListOfGifs from './components/ListOfGifs'
-import {Route} from 'wouter';
+import {Link, Route} from 'wouter';
 
 
 
@@ -10,6 +10,11 @@ function App() {
     <div className="App">
       <section className="App-content">
         <h1>App</h1>
+        <Link to='/gif/panda'> Gifs de pandas </Link>
+        <Link to='/gif/perros'> Gifs de perros </Link>
+        <Link to='/gif/Rick'> Gifs de Rick </Link>
+        <Link to='/gif/Morty'> Gifs de Morty </Link>
+        <Link to='/gif/friends'> Gifs de friends </Link>
         <Route path = "/gif/:keyword" 
         component= {ListOfGifs}/>
         {/* <ListOfGifs keyword='morty'/> */}
